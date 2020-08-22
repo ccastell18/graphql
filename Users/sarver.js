4e6cc32d4886a7e8fb083f655f4fa4582da56a74
Christopher Castellano <c.castellano18@gmail.com>
2020-08-22 01:47:44 -0500

    first Schema

--- Users/sarver.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-
-const app = express();
-
-app.listen(4000, () => {
-  console.log('Server Listening on 4000');
-});
---
+++ Users/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
+
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
