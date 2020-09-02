250312456aaea12b1133ebd1044dc567680f9400
Christopher Castellano <c.castellano18@gmail.com>
2020-09-02 01:01:26 -0500

    restart

--- graphql_basics/src/resolvers/User.js
+++
@@ -1,9 +0,0 @@
-const User = {
-  posts(parent, args, { db }, info) {
-    return db.posts.filter((post) => {
-      return post.author === parent.id;
-    });
-  },
-};
-
-export { User as default };
